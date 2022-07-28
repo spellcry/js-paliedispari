933bac7c0f95cc3a51626ebce40eac99923b0a15
spellcry <sangiothebest@hotmail.com>
2022-07-28 16:20:16 +0200

    add soluzione pari o dispari

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,48 @@ function isPalindroma(parolaDaInvertire) {
     return parolaInvertita === parolaDaInvertire;
 }
 
+// funzione che restituisce un numero random compreso tra due estremi inclusi
+function numeroRandom(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// funzione pari o dispari
+function pariDispari (numeroUno, numeroDue) {
+    const sommaNumeri = numeroUno + numeroDue;
+    if ( sommaNumeri % 2 === 0 ) {
+        return 'pari';
+    }
+    return 'dispari';
+}
+
 // invocazione funzioni
 const parola = prompt('Inserisci una parola');
-console.log(isPalindroma(parola));
+if (isPalindroma(parola)) {
+    console.log(`La parola '${parola}' è palindroma!`);
+} else {
+    console.log(`La parola '${parola}' non è palindroma!`);
+}
+
+const sceltaUtente = prompt('Scegli e quindi scrivi \'pari\' o \'dispari\'');
+if ( sceltaUtente === 'pari' || sceltaUtente === 'dispari' ) {
+    console.log(`L'utente ha scelto: ${sceltaUtente}`);
+    const numeroUtente = parseInt(prompt('Scegli un numero da 1 a 5, estremi inclusi'));
+    if ( !isNaN(numeroUtente) && numeroUtente >= 1 && numeroUtente <= 5 ) {
+        console.log(`Il numero scelto dall'utente è: ${numeroUtente}`);
+        const numeroComputerRandom = numeroRandom(1, 5);
+        console.log(`Il numero del computer è: ${numeroComputerRandom}`);
+        const sommaPariDispari = pariDispari(numeroUtente, numeroComputerRandom);
+        console.log(`La somma ${numeroUtente} + ${numeroComputerRandom} = ${numeroUtente + numeroComputerRandom} è: ${sommaPariDispari}`);
+        if ( sommaPariDispari === sceltaUtente ) {
+            console.log('Ha vinto l\'utente!');
+        } else {
+            console.log('Ha vinto il computer!');
+        }
+    } else {
+        console.warn('Devi inserire un numero tra 1 e 5, estremi inclusi');
+    }
+} else {
+    console.warn('Devi scrive \'pari\' o \'dispari\'! Gioco terminato.');
+}
